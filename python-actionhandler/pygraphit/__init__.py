0c800baa3e4cfc160e8179a77925cea95ea95ded
stormking <stormking@web.de>
2016-10-12 11:25:35 +0200

    Import correct class

--- python-actionhandler/pygraphit/__init__.py
+++ python-actionhandler/pygraphit/__init__.py
@@ -1,5 +1,5 @@
 from pygraphit.node import GraphitNode
 from pygraphit.session import GraphitSession
-from pygraphit.wso2auth import WSO2Auth, WSO2AuthCC
+from pygraphit.wso2auth import WSO2AuthPW, WSO2AuthCC
 
 from pygraphit.exceptions import GraphitError, WSO2Error, WSO2TokenRenewalError
