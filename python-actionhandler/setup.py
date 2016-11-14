2184fdcd3644f2b7a52821f1768b122314855408
stormking <stormking@web.de>
2016-11-14 14:46:35 +0100

    Add support for EyeShareWebService_Email_Format and EyeshareWebService_Generic operations

--- python-actionhandler/setup.py
+++ python-actionhandler/setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "pyactionhandler",
-    version = "1.0.5",
+    version = "1.1.0",
     author = "Marcus Klemm",
     author_email = "mklemm@arago.de",
     description = ("Python library for Arago HIRO ActionHandlers"),
