196501e37fe73f62d2c9a3e636725e800e277a6c
stormking <stormking@web.de>
2017-02-19 21:41:32 +0100

    Check Content-type header, only accept JSON

--- connectit-netcool-adapter.py
+++ connectit-netcool-adapter.py
@@ -41,6 +41,17 @@ class SOAPLogger(zeep.Plugin):
 					envelope, encoding='unicode', pretty_print=True))
 		return envelope, http_headers
 
+class RequireJSON(object):
+	def process_request(self, req, resp):
+		if not req.client_accepts_json:
+			raise falcon.HTTPNotAcceptable(
+				'This API only supports responses encoded as JSON.')
+
+		if req.method in ('POST', 'PUT'):
+			if 'application/json' not in req.content_type:
+				raise falcon.HTTPUnsupportedMediaType(
+					'This API only supports requests encoded as JSON.')
+
 class RESTAPI(object):
 	def __init__(self, baseurl, endpoint):
 		self.app=falcon.API()
