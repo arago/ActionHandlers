8923a25aa8badbd43961f6abe8c17a478fd3226d
stormking <stormking@web.de>
2017-02-19 21:42:30 +0100

    Move JSON validation to middleware class

--- connectit-netcool-adapter.py
+++ connectit-netcool-adapter.py
@@ -52,6 +52,32 @@ class RequireJSON(object):
 				raise falcon.HTTPUnsupportedMediaType(
 					'This API only supports requests encoded as JSON.')
 
+class JSONTranslator(object):
+
+	def process_request(self, req, resp):
+		if req.content_length in (None, 0): return
+
+		body = req.stream.read()
+		if not body:
+			raise falcon.HTTPBadRequest(
+				'Empty request body',
+				'A valid JSON document is required.')
+		try:
+			req.context['doc'] = json.loads(body.decode('utf-8'))
+		except (ValueError, UnicodeDecodeError):
+			raise falcon.HTTPError(
+				falcon.HTTP_753,
+				'Malformed JSON',
+				'Could not decode the request body. The '
+				'JSON was incorrect or not encoded as '
+				'UTF-8.')
+
+	def process_response(self, req, resp, resource):
+		if 'result' not in req.context:
+			return
+
+		resp.body = json.dumps(req.context['result'])
+
 class RESTAPI(object):
 	def __init__(self, baseurl, endpoint):
 		self.app=falcon.API()
