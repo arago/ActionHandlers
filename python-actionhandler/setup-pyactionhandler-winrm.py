4a113ac686820b9de9a65718ef054641a33b56bd
stormking <stormking@web.de>
2017-03-01 10:07:33 +0100

    Separate winrm libs and script

--- python-actionhandler/setup-pyactionhandler-winrm.py
+++ python-actionhandler/setup-pyactionhandler-winrm.py
@@ -8,16 +8,15 @@ import distutils.core
 name = 'arago-pyactionhandler-winrm'
 
 distutils.core.setup(
-	name = "arago-pyactionhandler-winrm",
+	name = name,
 	version = "2.0",
 	author = "Marcus Klemm",
 	author_email = "mklemm@arago.de",
-	description = ("ActionHandler for Microsoft Windows"),
+	description = ("WinRM module for pyactionhandler"),
 	license = "MIT",
 	url = "http://www.arago.de",
 	long_description="""\
-Execute cmd.exe and powershell commands on remote
-Windows hosts via the WinRM protocol.
+Support for the Windows Remote Management protocol.
 """,
 	classifiers=[
 		"Development Status :: 5 - Production/Stable",
@@ -27,25 +26,5 @@ Windows hosts via the WinRM protocol.
 	packages=['arago.pyactionhandler.plugins.winrm',
 			  'arago.pyactionhandler.plugins.winrm.auth.kerberos'
 	],
-	install_requires=['arago-pyactionhandler'],
-	scripts=['bin/hiro-winrm-actionhandler.py'],
-	data_files=[
-		(
-			'/opt/autopilot/conf/external_actionhandlers/',
-			[
-				'config/winrm-actionhandler.conf',
-				'config/winrm-actionhandler-log.conf'
-			]
-		),
-		(
-			'/opt/autopilot/conf/external_actionhandlers/capabilities/',
-			[
-				'config/capabilities/winrm-actionhandler.xml',
-				'config/capabilities/winrm-actionhandler.yaml'
-			]
-		),
-		(
-			'/etc/init.d/', ['etc/init.d/hiro-winrm-actionhandler']
-		)
-	]
+	install_requires=['arago-pyactionhandler']
 )
---
+++ python-actionhandler/setup-winrm-actionhandler.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import os
+if os.environ.get('USER','') == 'vagrant':
+    del os.link
+
+import distutils.core
+
+name = 'winrm-actionhandler'
+
+distutils.core.setup(
+	name = name,
+	version = "2.0",
+	author = "Marcus Klemm",
+	author_email = "mklemm@arago.de",
+	description = ("ActionHandler for Microsoft Windows"),
+	license = "MIT",
+	url = "http://www.arago.de",
+	long_description="""\
+Execute cmd.exe and powershell commands on remote
+Windows hosts via the WinRM protocol.
+""",
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Topic :: Utilities",
+		"License :: OSI Approved :: MIT License",
+	],
+	install_requires=['arago-pyactionhandler-winrm'],
+	scripts=['bin/hiro-winrm-actionhandler.py'],
+	data_files=[
+		(
+			'/opt/autopilot/conf/external_actionhandlers/',
+			[
+				'config/winrm-actionhandler.conf',
+				'config/winrm-actionhandler-log.conf'
+			]
+		),
+		(
+			'/opt/autopilot/conf/external_actionhandlers/capabilities/',
+			[
+				'config/capabilities/winrm-actionhandler.xml',
+				'config/capabilities/winrm-actionhandler.yaml'
+			]
+		),
+		(
+			'/etc/init.d/', ['etc/init.d/hiro-winrm-actionhandler']
+		)
+	]
+)
