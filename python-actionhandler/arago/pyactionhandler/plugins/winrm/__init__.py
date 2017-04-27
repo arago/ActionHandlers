17297403d8b8d572cbe3bd566667a9992c192ca8
mklemm2 <mklemm@arago.de>
2017-04-27 13:24:46 +0200

    Add parameters for Keytab management

--- python-actionhandler/arago/pyactionhandler/plugins/winrm/__init__.py
+++ python-actionhandler/arago/pyactionhandler/plugins/winrm/__init__.py
@@ -6,6 +6,7 @@ from arago.pyactionhandler.plugins.winrm.auth.kerberos import krb5Session
 from arago.pyactionhandler.plugins.winrm.script import Script
 
 import winrm.exceptions
+import requests.exceptions
 import arago.pyactionhandler.plugins.winrm.exceptions
 import logging
 import re
@@ -54,18 +55,26 @@ class WinRMCmdAction(Action):
 				self.system_rc=-1
 				if parsed_err == "Ticket expired" or (parsed_err.startswith("Credentials cache file ") and parsed_err.endswith(" not found")):
 					self.logger.warning("[{anum}] No Kerberos ticket for {host}, requesting one ...".format(anum=self.num, host=self.parameters['Hostname']))
+					self.statusmsg="Authentication failed."
 					try:
+						with open(self.parameters['Keytab']):
+							pass
 						gevent.subprocess.check_output(["kinit", "-k", "-t", self.parameters['Keytab'], self.parameters['Username']], timeout=30, stderr=gevent.subprocess.STDOUT)
 						self.logger.info("[{anum}] Successfully retrieved Kerberos ticket for {host}, retrying command execution.".format(anum=self.num, host=self.parameters['Hostname']))
 					except gevent.subprocess.CalledProcessError as e:
 						self.logger.error("[{anum}] Retrieving Kerberos ticket for {host} failed: {err}".format(anum=self.num, host=self.parameters['Hostname'], err=e.output.decode("utf-8")))
 					except gevent.subprocess.TimeoutExpired as e:
 						self.logger.error("[{anum}] Retrieving Kerberos ticket for {host} timed out!".format(anum=self.num, host=self.parameters['Hostname']))
+					except FileNotFoundError as e:
+						self.logger.error("[{anum}] Retrieving Kerberos ticket for {host} failed: {err}".format(anum=self.num, host=self.parameters['Hostname'], err=e))
 					except KeyError:
 						self.logger.error("[{anum}] Retrieving Kerberos ticket for {host} failed! Credentials missing!".format(anum=self.num, host=self.parameters['Hostname']))
 					continue
-				else:
+				elif parsed_err:
 					self.logger.error("[{anum}] An error occured during command execution on {node}: Kerberos: {err}".format(anum=self.num, node=self.node,err=parsed_err))
+					self.statusmsg=parsed_err
+				else:
+					self.logger.error("[{anum}] An error occured during command execution on {node}: Kerberos: {err}".format(anum=self.num, node=self.node,err=str(e)))
 			except (winrm.exceptions.WinRMError, winrm.exceptions.WinRMTransportError, arago.pyactionhandler.plugins.winrm.exceptions.WinRMError, requests.exceptions.ConnectTimeout) as e:
 				self.statusmsg=str(e)
 				self.success=False
@@ -74,10 +83,13 @@ class WinRMCmdAction(Action):
 			break
 
 	def __call__(self):
-		winrm_session=self.init_direct_session(
-			host = self.parameters['Hostname'],
-			protocol = 'https' if self.parameters['UseSSL'] == 'true' else 'http',
-			port = '5986' if self.parameters['UseSSL'] == 'true' else '5985')
+		try:
+			winrm_session=self.init_direct_session(
+				host = self.parameters['Hostname'],
+				protocol = 'https' if self.parameters['UseSSL'] == 'true' else 'http',
+				port = '5986' if self.parameters['UseSSL'] == 'true' else '5985')
+		except KeyError:
+			self.logger.error("[{anum}] Parameter 'UseSSL' is missing, check your CapabilityYAML file")
 		self.logger.debug("[{anum}] Connecting directly to '{target}'".format(
 			anum=self.num,
 			target=self.parameters['Hostname']))
