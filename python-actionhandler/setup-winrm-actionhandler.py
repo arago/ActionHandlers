9a0aa07739e1426435ce12debcec62941ede2956
mklemm2 <mklemm@arago.de>
2017-06-01 12:38:19 +0200

    Bump version

--- python-actionhandler/setup-winrm-actionhandler.py
+++ python-actionhandler/setup-winrm-actionhandler.py
@@ -9,7 +9,7 @@ name = 'winrm-actionhandler'
 
 distutils.core.setup(
 	name = name,
-	version = "2.2",
+	version = "2.3",
 	author = "Marcus Klemm",
 	author_email = "mklemm@arago.de",
 	description = ("ActionHandler for Microsoft Windows"),
